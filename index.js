15c6702dffb9842b7b0e2c4f0f55f45667e1f3b0
Nirada Sukchit <59160104@go.buu.ac.th>
2019-01-18 16:39:50 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,24 @@
+const sum = require("./sum")
+console.log(sum(1, 2))
+
+console.log(sum(21, 1))
+
+console.log(sum(2, 5))
+
+console.log(sum(10, 25))
+
+console.log(sum(11, 22))
+
+console.log(sum(100, 200))
+
+
+const sum = require("./minus")
+console.log(min(20, 1))
+
+console.log(min(20, 5))
+
+console.log(min(10, 2))
+
+console.log(min(100, 20))
+
+console.log(min(18, 8))
